764778014297375a8b87eecf28ed770e5b427a6b
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2023-08-20 10:19:08 +0500

    Add files via upload

---
+++ PensionData.cpp
@@ -0,0 +1,200 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    float age;
+    int retire;
+    int current_possition;
+                            int a=2000 , b=2500 , c=2000 , d=1500 , e=1700,
+                                f=1500 , g=2100 , h=3000 , i=5000 , j=4300,
+                                k=3000 , l=3500 , m=3600 , n=4000 , o=4500;
+
+
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                 *************************************************      "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                               Enter Age On First Line                  "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                          Enter Retirement Age On Second Line           "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                                                                        "<<endl;
+    cout<<"                                 *************************************************      "<<endl;
+    cin>>age;
+    cin>>retire;
+
+
+
+
+    current_possition= age - retire;
+    cout<<"                        "<<endl;
+    cout<<"                        "<<endl;
+    cout<<"Current Pension Age is: "<<current_possition<<endl;
+    cout<<"                        "<<endl;
+    cout<<"         ****           "<<endl;
+
+
+
+    if(current_possition == 15 ){
+        double  totalPension_15 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12) + (k*12) + (l*12) + (m*12) + (n*12) + (o*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_15<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+       }
+        else if(current_possition == 14){
+        double totalPension_14 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12) + (k*12) + (l*12) + (m*12) + (n*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_14<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 13){
+        double totalPension_13 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12) + (k*12) + (l*12) + (m*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_13<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 12){
+        double totalPension_12 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12) + (k*12) + (l*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_12<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 11){
+        double totalPension_11 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12) + (k*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_11<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+
+        else if(current_possition == 10){
+        double totalPension_10 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12) + (j*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_10<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 9){
+        double totalPension_9 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12) + (i*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_9<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 8){
+        double totalPension_8 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12) + (h*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_8<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 7){
+        double totalPension_7 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12) + (g*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_7<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 6){
+        double totalPension_6 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12) + (f*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_6<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 5){
+        double totalPension_5 = (a*12) + (b*12) + (c*12) + (d*12) + (e*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_5<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 4){
+        double totalPension_4 = (a*12) + (b*12) + (c*12) + (d*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_4<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+
+        else if(current_possition == 3){
+        double totalPension_3 = (a*12) + (b*12) + (c*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_3<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if(current_possition == 2){
+        double totalPension_2 = (a*12) + (b*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_2<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        else if (current_possition == 1){
+        double totalPension_1 = (a*12);
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"Total Pension he took from government is = "<<totalPension_1<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"                                           "<<endl;
+        cout<<"*******************************************"<<endl;
+        }
+
+        return 0;
+
+       }
+
+
+
